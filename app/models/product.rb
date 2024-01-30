e93a8986ca6fbaabceea60bbef50ae28ebcb3b97
Brian Tuohy <BTcodez22@gmail.com>
2024-01-29 19:36:17 -0600

    ex2 end commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: {maximum: 30}
+    validates :price, presence: true, numericality: {greater_than: 0}
 end
