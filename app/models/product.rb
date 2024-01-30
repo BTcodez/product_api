386d9e18969d0c80fd7ae4751db813802840120e
Brian Tuohy <BTcodez22@gmail.com>
2024-01-29 20:19:48 -0600

    end of class on ex3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+
     validates :name, presence: true, uniqueness: true, length: {maximum: 30}
     validates :price, presence: true, numericality: {greater_than: 0}
 end
